ef41de0fac54fec3250a45b6636ac7ef484496d3
Carlos Mertens <mertenscarlos@yahoo.com>
2024-11-04 16:28:28 +0100

    primitive data and props

---
+++ src/components/Info.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let name;
+	export let age;
+	export let city;
+</script>
+
+<p>My name is {name} and I am {age} years old. I live in {city}.</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,64 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	// Exercise 1
+	let count = 0;
+	$: doubled = count * 2;
+
+	$: if (count >= 5) {
+		alert('count is too high!');
+		count = 0;
+	}
+
+	function increment() {
+		count += 1;
+	}
+
+	// Exercise 2
+	let numbers = [1, 2, 3, 4];
+
+	function addNumber() {
+		numbers.push(numbers.length + 1);
+		numbers = numbers;
+	}
+
+	$: sum = numbers.reduce((total, num) => total + num, 0);
+
+	// Exercise 3
+	import Nested from '../components/Nested.svelte';
+
+	// Exercise 4
+	import Info from '../components/Info.svelte';
+
+	let data = {
+		name: 'Carlos',
+		age: 44,
+		city: 'Berlin'
+	};
+</script>
+
+<h1 class="rounded-md text-center text-3xl">Welcome to SvelteKit</h1>
+
+<div class="mb-8">
+	<h2>Exercise 1</h2>
+	<p>{count} doubles is {doubled}</p>
+	<button on:click={increment} class="rounded-md bg-green-500 px-2">
+		Clicked {count}
+		{count === 1 ? 'time' : 'times'}
+	</button>
+</div>
+
+<div class="mb-8">
+	<h2>Exercise 2</h2>
+	<p>{numbers.join(' + ')} = {sum}</p>
+	<button on:click={addNumber} class="rounded-md bg-blue-500 px-2">Add a number</button>
+</div>
+
+<div class="mb-8">
+	<h2>Exercise 3</h2>
+	<Nested answer={'44'} />
+	<Nested />
+</div>
+
+<div>
+	<h2>Exercise 4</h2>
+	<Info {...data} />
+</div>
