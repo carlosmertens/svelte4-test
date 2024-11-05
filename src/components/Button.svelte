023836c106515a84b0db069a6c021fcfd9f1a360
Carlos Mertens <mertenscarlos@yahoo.com>
2024-11-05 10:26:01 +0100

    logic conditions

---
+++ src/components/Button.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let text;
+	export let onClick;
+</script>
+
+<button on:click={onClick} class="rounded-md bg-blue-400 px-2 hover:bg-blue-300">{text}</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Button from '../components/Button.svelte';
+
 	// Exercise 1
 	let count = 0;
 	$: doubled = count * 2;
@@ -33,32 +35,105 @@
 		age: 44,
 		city: 'Berlin'
 	};
+
+	// Exercise 5
+	let user = { loggedIn: false };
+	let x = 7;
+
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
+
+	// Exercise 6
+	let cats = [
+		{ id: 'J---aiyznGQ', name: 'Keyboard Cat' },
+		{ id: 'z_AbfPXTKms', name: 'Maru' },
+		{ id: 'OUtn3pvWmpg', name: 'Henri The Existential Cat' }
+	];
+
+	// Exercise 7
+	import Thing from '../components/Thing.svelte';
+
+	let things = [
+		{ id: 1, name: 'apple' },
+		{ id: 2, name: 'banana' },
+		{ id: 3, name: 'carrot' },
+		{ id: 4, name: 'doughnut' },
+		{ id: 5, name: 'egg' }
+	];
+
+	function handleRemove() {
+		things = things.slice(1);
+	}
 </script>
 
 <h1 class="rounded-md text-center text-3xl">Welcome to SvelteKit</h1>
 
 <div class="mb-8">
-	<h2>Exercise 1</h2>
+	<h2 class="text-lg font-bold">Exercise 1</h2>
 	<p>{count} doubles is {doubled}</p>
-	<button on:click={increment} class="rounded-md bg-green-500 px-2">
-		Clicked {count}
-		{count === 1 ? 'time' : 'times'}
-	</button>
+	<Button text={`Clicked ${count} ${count === 1 ? 'time' : 'times'}`} onClick={increment} />
 </div>
 
 <div class="mb-8">
-	<h2>Exercise 2</h2>
+	<h2 class="text-lg font-bold">Exercise 2</h2>
 	<p>{numbers.join(' + ')} = {sum}</p>
-	<button on:click={addNumber} class="rounded-md bg-blue-500 px-2">Add a number</button>
+	<Button text="Add a number" onClick={addNumber} />
 </div>
 
 <div class="mb-8">
-	<h2>Exercise 3</h2>
+	<h2 class="text-lg font-bold">Exercise 3</h2>
 	<Nested answer={'44'} />
 	<Nested />
 </div>
 
-<div>
-	<h2>Exercise 4</h2>
+<div class="mb-8">
+	<h2 class="text-lg font-bold">Exercise 4</h2>
 	<Info {...data} />
+</div>
+
+<div class="mb-8">
+	<h2 class="text-lg font-bold">Exercise 5</h2>
+
+	{#if user.loggedIn}
+		<Button text="Log out" onClick={toggle} />
+	{/if}
+
+	{#if !user.loggedIn}
+		<Button text="Log in" onClick={toggle} />
+	{/if}
+
+	{#if x > 10}
+		<p>{x} is greater than 10</p>
+	{:else if 5 > x}
+		<p>{x} is less than 5</p>
+	{:else}
+		<p>{x} is between 5 and 10</p>
+	{/if}
+</div>
+
+<div class="mb-8">
+	<h2 class="text-lg font-bold">Exercise 6</h2>
+	<ul>
+		{#each cats as cat}
+			<li>
+				<a
+					target="_blank"
+					href="https://www.youtube.com/watch?v={cat.id}"
+					rel="noreferrer"
+					class="text-blue-950 underline">{cat.name}</a
+				>
+			</li>
+		{/each}
+	</ul>
+</div>
+
+<div>
+	<h2>Exercise 7</h2>
+
+	<Button onClick={handleRemove} text="Remove first thing" />
+
+	{#each things as thing (thing.id)}
+		<Thing name={thing.name} />
+	{/each}
 </div>
